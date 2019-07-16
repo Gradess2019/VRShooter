a519894a0121c937db22a5e23fff92d1a265fe3d
Stepan <trofimovjava@gmail.com>
2019-07-16 15:27:54 +0300

    Add base components creation
    Save NewMap

--- Source/Shooter/Private/PlayerPawn.cpp
+++ Source/Shooter/Private/PlayerPawn.cpp
@@ -2,25 +2,62 @@
 
 #include "Public/PlayerPawn.h"
 #include "Components/InputComponent.h"
+#include "Camera/CameraComponent.h"
+#include "MotionControllerComponent.h"
+#include "HeadMountedDisplayFunctionLibrary.h"
+
+
+DEFINE_LOG_CATEGORY(PlayerPawnLog);
+#define LOG(Message) UE_LOG(PlayerPawnLog, Log, TEXT(Message))
+
+APlayerPawn::APlayerPawn(const FObjectInitializer& ObjectInitializer) : Super(ObjectInitializer)
+{
+	InitializeBaseComponents();
+}
+
+void APlayerPawn::InitializeBaseComponents()
+{
+	RootComponent = CreateDefaultSubobject<USceneComponent>(TEXT("RootComponent"));
+
+	VROrigin = CreateComponent<USceneComponent>("VROrigin", RootComponent);
+	cameraComponent = CreateComponent<UCameraComponent>("VRCamera", VROrigin);
+	leftController = CreateComponent<UMotionControllerComponent>("LeftMotionController", VROrigin);
+	rightController = CreateComponent<UMotionControllerComponent>("RightMotionController", VROrigin);
+}
+
+template<class T>
+T* APlayerPawn::CreateComponent(const FName& COMPONENT_NAME, USceneComponent* parent)
+{
+	checkf(parent, TEXT("Parent is null"));
+	T* newComponent = CreateDefaultSubobject<T>(COMPONENT_NAME);
+	newComponent->SetupAttachment(parent);
+	return newComponent;
+}
 
 void APlayerPawn::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
 {
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
-	//PlayerInputComponent->BindAction("FireLeft", EInputEvent::IE_Pressed, );
+
+	check(PlayerInputComponent);
+
+	PlayerInputComponent->BindAction("FireLeft", EInputEvent::IE_Pressed, this, &APlayerPawn::FireLeft);
+	PlayerInputComponent->BindAction("FireRight", EInputEvent::IE_Pressed, this, &APlayerPawn::FireRight);
 }
 
-void APlayerPawn::PostInitializeComponents()
+void APlayerPawn::BeginPlay()
 {
-	
+	Super::BeginPlay();
+
+	UHeadMountedDisplayFunctionLibrary::SetTrackingOrigin(EHMDTrackingOrigin::Floor);
 }
 
 void APlayerPawn::FireLeft()
 {
-	UE_LOG(LogTemp, Warning, TEXT("Fire Left"));
+	LOG("Fire Left");
 }
 
 void APlayerPawn::FireRight()
 {
-	UE_LOG(LogTemp, Warning, TEXT("Fire Right"));
+	LOG("Fire Right");
 }
 
--- Source/Shooter/Public/PlayerPawn.h
+++ Source/Shooter/Public/PlayerPawn.h
@@ -7,6 +7,8 @@
 #include "Weapon.h"
 #include "PlayerPawn.generated.h"
 
+DECLARE_LOG_CATEGORY_EXTERN(PlayerPawnLog, Log, All);
+
 UCLASS()
 class SHOOTER_API APlayerPawn : public APawn
 {
@@ -14,13 +16,38 @@ class SHOOTER_API APlayerPawn : public APawn
 
 public:
 
-	void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+	APlayerPawn(const FObjectInitializer& ObjectInitializer);
 
-	void PostInitializeComponents() override;
+protected:
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Pawn settings")
+	USceneComponent* VROrigin;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly,Category = "Pawn settings")
+	class UCameraComponent* cameraComponent;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Pawn settings")
+	class UMotionControllerComponent* leftController;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Pawn settings")
+	class UMotionControllerComponent* rightController;
 
 private:
-	
+
+	void InitializeBaseComponents();
+
+	template<class T>
+	T* CreateComponent(const FName& COMPONENT_NAME, USceneComponent* parent);
+
+	UFUNCTION()
+	void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+	UFUNCTION()
+	void BeginPlay() override;
+
 	void FireLeft();
+	
+	UFUNCTION()
 	void FireRight();
 
 };
