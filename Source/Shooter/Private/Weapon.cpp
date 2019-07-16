ccce7a561e6c443ccd7836d0fbd121a57714a12f
Stepan <trofimovjava@gmail.com>
2019-07-16 16:04:49 +0300

    Add base weapon class

--- Source/Shooter/Private/Weapon.cpp
+++ Source/Shooter/Private/Weapon.cpp
@@ -2,3 +2,7 @@
 
 #include "Public/Weapon.h"
 
+void AWeapon::Fire()
+{
+	unimplemented();
+}
--- Source/Shooter/Public/Weapon.h
+++ Source/Shooter/Public/Weapon.h
@@ -6,10 +6,19 @@
 #include "GameFramework/Actor.h"
 #include "Weapon.generated.h"
 
-UCLASS(Abstract)
+UCLASS(Abstract, BlueprintType, Blueprintable)
 class SHOOTER_API AWeapon : public AActor
 {
 	GENERATED_BODY()
-	
+
+public:
+
+	UFUNCTION()
+	virtual void Fire();
+
+protected:
+
+	UPROPERTY(VisibleAnywhere, Category = "Weapon settings")
+	UStaticMeshComponent* mesh;
 
 };
